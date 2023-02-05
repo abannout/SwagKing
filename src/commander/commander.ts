3a6cc75b58aabe91248792dd38830df57fb656d2
thund <2506143-thund@users.noreply.gitlab.com>
2023-02-05 18:32:09 +0100

    refactor to command object

--- src/commander/commander.ts
+++ src/commander/commander.ts
@@ -10,15 +10,34 @@ import { CommandFunction, CommanderNotification } from "../types";
 
 // import { bank, fleet, map, price, radar } from "../state/state";
 
-const commands: CommandFunction[] = [];
+// Each round we can issue commands on per-robot basis and
+// one global command
+type CommandRegistry = {
+  global?: CommandFunction;
+  robots: Record<string, CommandFunction>;
+};
+const commands: CommandRegistry = {
+  robots: {},
+};
+
+function clearRegistry() {
+  delete commands.global;
+  commands.robots = {};
+}
 
 export function fetchCommands(): CommandFunction[] {
-  return [...commands];
+  const arr = [];
+  if (commands.global) {
+    arr.push(commands.global);
+  }
+  arr.push(...Object.values(commands.robots));
+  clearRegistry();
+  return arr;
 }
 
 export function notify(notification: CommanderNotification) {
   // First action is buying robots
   if (notification.type === "game" && notification.status === "started") {
-    commands.push(() => buyRobots(5));
+    commands.global = () => buyRobots(5);
   }
 }
