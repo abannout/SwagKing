9d532b1135d86456d7226e7b8460f42ca43ab72b
thund <2506143-thund@users.noreply.gitlab.com>
2023-02-25 18:11:25 +0100

    move strategy logic in module

--- src/commander/commander.ts
+++ src/commander/commander.ts
@@ -10,18 +10,10 @@ import { CommandFunction, CommanderNotification } from "../types";
 
 import { FleetedRobot } from "../state/fleet.js";
 import { bank, fleet, map, price } from "../state/state.js";
-import { StrategyType, strategies } from "./strategies/strategies.js";
+import * as strategies from "./strategies/strategies.js";
 
 const MAX_FLEET_SIZE = 30;
 const DEFAULT_ROBOT_BUY_BATCH_SIZE = 5;
-
-const strategyDistribution: Record<StrategyType, number> = {
-  FARMING: 0.8,
-  FIGHTING: 0,
-  EXPLORING: 0.2,
-};
-
-const strategyAssignment: Record<string, StrategyType> = {};
 
 export const IDLE_ACTION = (robot: FleetedRobot): CommandFunction => {
   const path = map.shortestPathToUnknownPlanet(robot.planet);
@@ -52,61 +44,12 @@ function globalCommands(): CommandFunction[] {
   return arr;
 }
 
-function validateStrategyDistribution() {
-  const sum = Object.values(strategyDistribution).reduce(
-    (acc, val) => acc + val,
-    0
-  );
-  if (sum !== 1) {
-    throw new Error("Strategy distribution does not sum to 1");
-  }
-}
-
-function getCurrentStratgeyDistribution(): Record<StrategyType, number> {
-  const currentDistribution: Record<StrategyType, number> = {
-    FARMING: 0,
-    FIGHTING: 0,
-    EXPLORING: 0,
-  };
-  const fleetSize = fleet.size();
-  if (fleetSize <= 0) return currentDistribution;
-
-  for (const robot of fleet.getAll()) {
-    const strategy = strategyAssignment[robot.id];
-    currentDistribution[strategy]++;
-  }
-
-  for (const strategy of Object.keys(currentDistribution)) {
-    currentDistribution[strategy as StrategyType] /= fleetSize;
-  }
-
-  return currentDistribution;
-}
-
-function getStrategyForRobot(robot: FleetedRobot): StrategyType {
-  validateStrategyDistribution();
-
-  if (!strategyAssignment[robot.id]) {
-    const currentDistribution = getCurrentStratgeyDistribution();
-    const targetDistribution = strategyDistribution;
-
-    for (const [key, value] of Object.entries(targetDistribution)) {
-      if (currentDistribution[key as StrategyType] < value) {
-        strategyAssignment[robot.id] = key as StrategyType;
-        return key as StrategyType;
-      }
-    }
-  }
-  return strategyAssignment[robot.id];
-}
-
 function robotCommands(): CommandFunction[] {
   const robotCmd: Record<string, CommandFunction> = {};
 
   for (const robot of fleet.getAll()) {
     const id = robot.id;
-    const strategyType = getStrategyForRobot(robot);
-    const strategy = strategies[strategyType];
+    const strategy = strategies.getStrategyForRobot(robot);
     robotCmd[id] = strategy.nextMove(robot) || IDLE_ACTION(robot);
   }
 
--- src/commander/strategies/strategies.ts
+++ src/commander/strategies/strategies.ts
@@ -1,4 +1,5 @@
 import { FleetedRobot } from "../../state/fleet.js";
+import { fleet } from "../../state/state.js";
 import { CommandFunction } from "../../types.js";
 
 import * as exploring from "./exploring.js";
@@ -13,8 +14,65 @@ export type Strategey = {
   nextMove: StrategyFunction;
 };
 
+const strategyDistribution: Record<StrategyType, number> = {
+  FARMING: 0.8,
+  FIGHTING: 0,
+  EXPLORING: 0.2,
+};
+
+const strategyAssignment: Record<string, StrategyType> = {};
+
 export const strategies: Record<StrategyType, Strategey> = {
   FARMING: farming,
   FIGHTING: fighting,
   EXPLORING: exploring,
 };
+
+function validateStrategyDistribution() {
+  const sum = Object.values(strategyDistribution).reduce(
+    (acc, val) => acc + val,
+    0
+  );
+  if (sum !== 1) {
+    throw new Error("Strategy distribution does not sum to 1");
+  }
+}
+
+function getCurrentStratgeyDistribution(): Record<StrategyType, number> {
+  const currentDistribution: Record<StrategyType, number> = {
+    FARMING: 0,
+    FIGHTING: 0,
+    EXPLORING: 0,
+  };
+  const fleetSize = fleet.size();
+  if (fleetSize <= 0) return currentDistribution;
+
+  for (const robot of fleet.getAll()) {
+    const strategy = strategyAssignment[robot.id];
+    currentDistribution[strategy]++;
+  }
+
+  for (const strategy of Object.keys(currentDistribution)) {
+    currentDistribution[strategy as StrategyType] /= fleetSize;
+  }
+
+  return currentDistribution;
+}
+
+export function getStrategyForRobot(robot: FleetedRobot): Strategey {
+  validateStrategyDistribution();
+  const assignment = strategyAssignment[robot.id];
+
+  if (!assignment) {
+    const currentDistribution = getCurrentStratgeyDistribution();
+    const targetDistribution = strategyDistribution;
+
+    for (const [key, value] of Object.entries(targetDistribution)) {
+      if (currentDistribution[key as StrategyType] < value) {
+        strategyAssignment[robot.id] = key as StrategyType;
+        strategies[key as StrategyType];
+      }
+    }
+  }
+  return strategies[assignment];
+}
